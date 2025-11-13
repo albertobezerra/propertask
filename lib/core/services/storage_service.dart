18a981885c8e1793907c7589ac283a72b2fd0474
Alberto Bezerra <43380864+albertobezerra@users.noreply.github.com>
2025-11-13 22:58:57 +0000

    Setando o cloudinary para usar futuramente...

--- lib/core/services/storage_service.dart
+++ lib/core/services/storage_service.dart
@@ -2,13 +2,20 @@ import 'package:cloudinary_public/cloudinary_public.dart';
 import 'package:image_picker/image_picker.dart';
 
 class StorageService {
-  final cloudinary = CloudinaryPublic('seu_cloud_name', 'seu_upload_preset');
+  final cloudinary = CloudinaryPublic(
+    'dqthwfibc', // seu cloud name do print
+    'ml_default', // seu upload preset do print
+  );
 
   Future<List<String>> uploadImages(List<XFile> files) async {
     final urls = <String>[];
     for (final file in files) {
       final response = await cloudinary.uploadFile(
-        CloudinaryFile.fromFile(file.path, folder: 'propertask/propriedades'),
+        CloudinaryFile.fromFile(
+          file.path,
+          folder:
+              'propertask/propriedades', // opcional: organiza suas imagens numa pasta
+        ),
       );
       urls.add(response.secureUrl);
     }
